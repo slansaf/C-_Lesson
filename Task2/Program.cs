ad804702f310ad01d8761db46929bc33b9ddc3e8
SlanSaf <slansaf@yandex.ru>
2023-12-20 18:22:10 +0300

    Add lesson1: Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l = 4;
+long sum2 = i1 + l; 
+Console.WriteLine(sum2);
+
+double d1 = 5.4;
+double sum3 = i1 + d1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l * i1;
+double product3 = i1 * d1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
