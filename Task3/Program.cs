9e64be5de36281534516693aef8ff4a892a24523
SlanSaf <slansaf@yandex.ru>
2023-12-20 20:19:50 +0300

    modificashin Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a  = 100;
+int b = 34;
+int c = 48;
+int d = 56;
+int e = 23;
+int max = a; 
+if (b > max){ max = b;}
+if (c > max){ max = c;}
+if (d > max){ max = d;}
+if (e > max){ max = e;}
+
+Console.WriteLine(max);
