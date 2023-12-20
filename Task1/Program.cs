a90e0aa389185140eb98835abe0500d6da8a9b78
SlanSaf <slansaf@yandex.ru>
2023-12-20 18:07:44 +0300

    Add some variables lesson1: task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World");
+﻿// Console.WriteLine(value);
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c  = 'H';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
