39a7ae1c3419e549b580e4b4641efc28aa153c5a
SlanSaf <slansaf@yandex.ru>
2024-01-02 22:38:49 +0300

    Изучена тема: работа со строками

---
+++ Lesson_2/Task2_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace prject
+{
+	class Program
+	{
+		static void Main()
+		{
+			//Работа со строками
+
+			string word = "Hello";
+
+			// System.Console.WriteLine(word.Length);
+			// word = String.Concat(word, "!!!");
+			// System.Console.WriteLine(word);
+			// System.Console.WriteLine(String.Compare(word, "Hello!!!"));
+
+			string people = "Sasha, Petay, Kostay";
+			string[] name = people.Split(new char[]{','});
+			PrintMas(name);
+			people = string.Join(" ", name);
+			System.Console.WriteLine(people);
+		}
+
+		static void Print(string word){
+			System.Console.WriteLine("Elem: " + word.Trim());
+		}
+
+		static void PrintMas(string[] word){
+			foreach (string el in word)
+				Print(el);
+		}
+	}
+}
