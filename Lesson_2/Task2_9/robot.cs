14b4af29410f3823796b77491708a11093daad85
SlanSaf <slansaf@yandex.ru>
2024-01-05 12:00:21 +0300

    ООП искользования конструктора

--- Lesson_2/Task2_9/robot.cs
+++ Lesson_2/Task2_9/robot.cs
@@ -4,10 +4,17 @@ namespace projact
 {
 	class Robot{
 
-		private string name;
-		private int weigth;
-		private byte[] coordinates;
+		private string name = "";
+		private int weigth = 0;
+		private byte[] coordinates = new byte[] {0, 0, 0};
 
+		public Robot(string _name, int _weigth, byte[] _coordinates) {
+			System.Console.WriteLine("Object has been created");
+			setValues(_name, _weigth, _coordinates);
+		}
+
+		public Robot() {}
+		
 		public void setValues(string _name, int _weigth, byte[] _coordinates){
 			name = _name;
 			weigth = _weigth;
