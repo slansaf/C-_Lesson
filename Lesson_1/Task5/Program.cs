f989cf1e208c71dc994481c09baf46323f0cb457
SlanSaf <slansaf@yandex.ru>
2024-01-11 22:19:07 +0300

    Добавлены файлы урокв Task5 и Task6 работа с одномерными массивами

---
+++ Lesson_1/Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace project
+{
+
+	class Program
+	{
+
+		static void Main()
+		{
+			int[] mas = new int[6] { 3, 5, 2, 8, 3, 2 };
+			System.Console.Write("Введите число: ");
+			int x = Convert.ToInt32(Console.ReadLine());
+			bool target = false;
+			foreach (int elem in mas)
+			{
+				if (x == elem)
+				{
+					System.Console.WriteLine($"Да есть такая цифра {elem}");
+					target = true;
+					break;
+				}
+
+			}
+			if (target == false)
+				System.Console.WriteLine("Такого числа нет");
+		}
+	}
+}
---
+++ Lesson_1/Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace project
+{
+
+	class Program
+	{
+
+		static void Main()
+		{
+			int[] mas = new int[6] { 3, -5, 2, -8, 3, -2 };
+			for( int i = 0; i < mas.Length; i++){
+					mas[i] *= -1;
+					System.Console.Write($"{mas[i]} ," );
+			}
+		}
+	}
+}
