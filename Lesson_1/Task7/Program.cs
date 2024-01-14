00fd1b57d7dd388a0cf40a7c67e5402077d61c80
SlanSaf <slansaf@yandex.ru>
2024-01-14 19:56:09 +0300

    Выполнил Д/з массивы, добавил решения задач в папку Lesson_1 -> Task6 and Task7

--- Lesson_1/Task7/Program.cs
+++ Lesson_1/Task7/Program.cs
@@ -4,39 +4,54 @@ using System.Linq;
 //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
 class UserInputToCompileForTest
 {
-	// Подсчет количества элементов массива, попадающих в заданный диапазон
-	// numbers - массив, в котором ведется подсчет
-	// minRange - минимальная граница диапазона
-	// maxRange - максимальная граница диапазона
-	public static int CountItemsRange(int[] numbers)
+	// Разница между максимальным и минимальным элементами массива
+
+	// Нахождение минимума массива
+	public static double FindMin(double[] numbers)
 	{
-		int numQuantity = 0;
-		for (int i = 0; i < numbers.Length; i++)
+		//Напишите свое решение здесь
+		double min = numbers[0];
+		foreach (float el in numbers)
 		{
-			if (numbers[i] % 2 == 0)
+			if (el < min)
 			{
-				numQuantity += 1;
+				min = el;
 			}
 		}
-		return numQuantity;
-
+		return (min);
 	}
 
-	public static void PrintResult(int[] array)
+	// Нахождение максимума массива
+	public static double FindMax(double[] numbers)
 	{
-		Console.WriteLine(CountItemsRange(array));
-		foreach
+		//Напишите свое решение здесь
+		double max = numbers[0];
+		foreach (float el in numbers)
+		{
+			if (el > max)
+			{
+				max = el;
+			}
+		}
+		return (max);
 	}
 
-}
 
+
+	public static void PrintResult(double[] array)
+	{
+		//Напишите свое решение здесь
+		double findeNum = FindMax(array) - FindMin(array);
+		Console.WriteLine(Math.Round(findeNum, 2));
+	}
+}
 
 //Не удаляйте и не меняйте класс Answer!
 class Answer
 {
 	public static void Main(string[] args)
 	{
-		int[] array;
+		double[] array;
 
 
 		if (args.Length >= 1)
@@ -46,7 +61,7 @@ class Answer
 
 			// Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
 			array = joinedArgs.Split(", ")
-										 .Select(int.Parse)
+										 .Select(double.Parse)
 										 .ToArray();
 
 			// Теперь arr содержит преобразованные в целые числа из командной строки
@@ -55,7 +70,7 @@ class Answer
 		else
 		{
 			// Если аргументов на входе нет
-			array = new int[] { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 }; // Создание массива
+			array = new double[] { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 }; // Создание массива
 		}
 		UserInputToCompileForTest.PrintResult(array);
 	}
