ed5e5c7ae5cdff5a4f62c4210caefc3a4f51b276
SlanSaf <slansaf@yandex.ru>
2024-01-15 20:10:03 +0300

    Lesson_3 -> Task1 - созданы функции по заполнению и выводу массива

---
+++ Lesson_3/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projact
+{
+	class Program
+	{
+		static void Main()
+		{	
+			System.Console.Write("Введите значение количества элементов массива: ");
+			int lim = Convert.ToInt32(Console.ReadLine());
+			int[] array = new int [lim];
+			NewArray(array);
+			PrintArray(array);
+		
+			
+		}
+
+		static void PrintArray(int[] arr){
+			foreach(int el in arr)
+				System.Console.Write($"{el} ");
+		}
+
+		static void NewArray(int[] arr)
+		{
+			Random rand = new Random();
+			for(int i = 0; i < arr.Length; i++){
+				arr[i] = rand.Next(-100, 100);
+			}
+		}
+
+	}
+
+
+}
