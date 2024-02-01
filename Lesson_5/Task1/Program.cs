b14da6f9dfa1e5c41d0b00388c7e96e58253683f
SlanSaf <slansaf@yandex.ru>
2024-02-01 21:41:51 +0300

    Введение в понятие Рекурсия

---
+++ Lesson_5/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace project
+{
+
+	class Program
+	{
+
+		static void Main()
+		{
+			char [] chars = {'a', '1', 'b', 'a', '!', '5'};
+			string str = GetStringFromArray(chars);
+			System.Console.WriteLine(str);
+
+			string str2 = "HellooooO World!";
+			char[] chars2 = StringToCharArray(str2);
+			System.Console.WriteLine($"[{string.Join(", ", chars2)}]");
+			System.Console.WriteLine(FindeVowelLetter(str2));
+
+		}
+
+		static string GetStringFromArray(char [] array){
+			string str = ""; //StringBuilder
+			foreach(char el in array){
+				str += el;
+			}
+			return str;
+		}
+
+		static char [] StringToCharArray(string str){
+			char[] chars = new char [str.Length];
+			for (int i = 0; i < str.Length; i++){
+				chars[i] = str[i];
+			}
+			return chars;
+		}
+
+		static int FindeVowelLetter(string str){
+			string vowels = "aoyuie";
+			int counter = 0;
+			foreach(char el in str.ToLower()){
+				if (vowels.Contains(el)){
+					counter++;
+				}
+			}
+			return counter;
+		}
+	}
+}
---
+++ Lesson_5/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace project
+{
+
+	class Program
+	{
+
+		static void Main()
+		{
+			int num = 23423;
+			int result = SummaDigitFromNumber(num);
+			System.Console.WriteLine(result);
+		}
+
+		static int  SummaDigitFromNumber(int num)
+		{
+			System.Console.WriteLine($"Call:{num}");
+			if (num < 10){
+				System.Console.WriteLine($"Hi, i am here");
+				return num;
+			}
+			int sum = num % 10;
+			int temp = SummaDigitFromNumber(num / 10);
+			System.Console.WriteLine($"Co back:{num}, {temp}");
+			sum+=temp;
+			return sum;
+		}
+
+	
+	}
+}
